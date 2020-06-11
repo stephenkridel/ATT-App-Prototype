2c5920255f880ab673c91aa162e104294c00e3cc
Stephen Kridel <stephen.kridel@gmail.com>
2020-06-10 22:55:56 -0400

    Removed all of the firebase scripts, firebase files, node modules, and references to them

---
+++ scripts/menu_screen.js
@@ -0,0 +1,26 @@
+var
+    toggler     = document.getElementById('toggler'),
+    menuScreen  = document.getElementById('menu-screen-div'),
+    xButton     = document.getElementById('x-button-menu')
+;
+
+// dims the page and shows the menu
+toggler.addEventListener('click', function(){
+    menuScreen.classList.remove('hide');
+    menuScreen.style.display = 'flex';
+    setTimeout(function(){
+        menuScreen.style.opacity = '1';
+        menuScreen.classList.add('show');
+    }, 10);
+});
+
+// dims the menu and shows the page
+xButton.addEventListener('click', function(){
+    menuScreen.classList.add('hide');
+    menuScreen.classList.remove('show');
+    menuScreen.style.opacity = '0';
+    setTimeout(function(){
+        menuScreen.style.display = 'none';
+    }, 300);
+});
+
